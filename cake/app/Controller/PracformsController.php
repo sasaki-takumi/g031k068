7a01c8e3b7413ed9abed82f160fd38d4d34c1a5a
macuser <you@example.com>
2013-11-11 20:43:07 +0900

    変更しました

--- cake/app/Controller/PracformsController.php
+++ cake/app/Controller/PracformsController.php
@@ -3,11 +3,9 @@
 		public $name = "Pracforms";
 		public $components = array('DebugKit.Toolbar');
 
-		public function index(){//アクション
+		public function index(){//indexアクション
 		}
 
-		public function result(){//アクション
-			// $tmp = $this->request->data['entry'];
-			// $this->set('user',$tmp);//ビューに値を受け渡す
+		public function result(){//resultアクション
 		}
 	}
